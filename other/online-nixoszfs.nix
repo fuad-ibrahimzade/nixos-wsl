17c52ece73e813e80227ded28670f07308b6e236
fuad.i <fuad.i@agilesolutions.az>
2021-08-29 15:40:32 +0400

    refactor ly git and lumina

--- other/online-nixoszfs.nix
+++ other/online-nixoszfs.nix
@@ -59,6 +59,24 @@ in
   time.timeZone = "Asia/Baku";
 
   # List services that you want to enable:
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+   services.xserver.enable = true;
+   services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+   services.xserver.libinput.enable = true;
+
+  # Enable Lumina desktop Environment
+  services.xserver.displayManager.ly.enable = true;
+  services.xserver.desktopManager.lumina.enable = true;
 
   # ZFS services
   services.zfs.autoSnapshot.enable = true;
