2ba8b7efcb0e63675d9d8cc7f2ac69e9ac6eb5ae
fuad.i <fuad.i@agilesolutions.az>
2021-09-04 23:06:13 +0400

    refactor

--- other/online-nixoszfs.nix
+++ other/online-nixoszfs.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let
   your_hostid = pkgs.writeShellScriptBin "your_hostid" ''
     echo $(head -c 8 /etc/machine-id)
